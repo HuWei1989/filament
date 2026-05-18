96dac628ef9914aac6b8008a269af8536310dd01
Powei Feng <powei@google.com>
2026-05-18 12:13:27 -0700

    vk: add offset to image blit api
     - Offset wasn't accounted for previously.
     - Failed backend test LoadImageTest.UpdateImage2D
     - Also fixed buffer overflow bug in adustedMemcpy where the pbd is
       larger than the mapped buffer.

--- filament/backend/test/test_LoadImage.cpp
+++ filament/backend/test/test_LoadImage.cpp
@@ -233,7 +233,6 @@ public:
 
 TEST_F(LoadImageTest, UpdateImage2D) {
     SKIP_IF(Backend::WEBGPU, "test cases fail in WebGPU, see b/424157731");
-    SKIP_IF(Backend::VULKAN, "b/453776547");
 
     // All of these test cases should result in the same rendered image, and thus the same hash.
     static const uint32_t expectedHash = 1875922935;
